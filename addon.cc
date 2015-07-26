4e1f7737b8bed81f75e93de8e3daf540a30d2f83
Sciss <stasgr@gmail.com>
2015-07-26 14:30:33 +0300

    alternative way using smalloc to create native float arrays

--- addon.cc
+++ addon.cc
@@ -15,24 +15,23 @@ NAN_METHOD(MatMul) {
 		NanReturnUndefined();
 	}
 
-	if (!args[0]->IsNumber() || !args[1]->IsNumber() || !args[2]->IsFloat32Array() ||
-		!args[3]->IsNumber() || !args[4]->IsNumber() || !args[5]->IsFloat32Array() ||
+	if (!args[0]->IsUint32() || !args[1]->IsUint32() || !args[2]->IsFloat32Array() ||
+		!args[3]->IsUint32() || !args[4]->IsUint32() || !args[5]->IsFloat32Array() ||
 		!args[6]->IsFloat32Array()) {
 		NanThrowTypeError("Wrong arguments");
 		NanReturnUndefined();
 	}
-	size_t rows1= size_t(args[0]->NumberValue());
-	size_t cols1 = size_t(args[1]->NumberValue());
+	size_t rows1(args[0]->Uint32Value());
+	size_t cols1(args[1]->Uint32Value());
 	Local<Float32Array> arr1 = args[2].As<Float32Array>();
 	Local<ArrayBuffer> buffer1 = arr1->Buffer();
 	ArrayBuffer::Contents contents1 = buffer1->Externalize();
-	std::cout << "alalal" << std::endl;
 	float* data1 = static_cast<float*>(contents1.Data());
 	std::cout << "After cast." << std::endl;
 	CMf first(data1, rows1, cols1);
 
-	size_t rows2 = size_t(args[3]->NumberValue());
-	size_t cols2 = size_t(args[4]->NumberValue());
+	size_t rows2(args[3]->Uint32Value());
+	size_t cols2(args[4]->Uint32Value());
 	Local<Float32Array> arr2 = args[5].As<Float32Array>();
 	Local<ArrayBuffer> buffer2 = arr2->Buffer();
 	ArrayBuffer::Contents contents2 = buffer2->Externalize();
@@ -52,8 +51,69 @@ NAN_METHOD(MatMul) {
 	NanReturnValue(num);
 }
 
+NAN_METHOD(MatMul2){
+	NanScope();
+	using CMf = Eigen::Map <const Eigen::MatrixXf >;
+	using Mf = Eigen::Map <Eigen::MatrixXf >;
+	//Map<MatrixXf> mf(pf, rows, columns);
+
+	if (args.Length() < 7) {
+		NanThrowTypeError("Wrong number of arguments");
+		NanReturnUndefined();
+	}
+
+	if (!args[0]->IsUint32() || !args[1]->IsUint32() ||/* !args[2]->IsFloat32Array() ||*/
+		!args[3]->IsUint32() || !args[4]->IsUint32()   /*||  !args[5]->IsFloat32Array() ||
+													   !args[6]->IsFloat32Array()*/) {
+		NanThrowTypeError("Wrong arguments");
+		NanReturnUndefined();
+	}
+	size_t rows1(args[0]->Uint32Value());
+	size_t cols1(args[1]->Uint32Value());
+	
+	
+	Local<Object> matrix1Buff = args[2].As<Object>();
+	float *data1 = nullptr;
+	size_t length1 = 0;
+
+	if (matrix1Buff->HasIndexedPropertiesInExternalArrayData()) {
+		length1 = size_t(matrix1Buff->GetIndexedPropertiesExternalArrayDataLength());
+		data1 = static_cast<float*>(matrix1Buff->GetIndexedPropertiesExternalArrayData());
+	}
+	CMf first(data1, rows1, cols1);
+
+	size_t rows2(args[3]->Uint32Value());
+	size_t cols2(args[4]->Uint32Value());
+
+	Local<Object> matrix2Buff = args[5].As<Object>();
+	float *data2 = nullptr;
+	size_t length2 = 0;
+
+	if (matrix2Buff->HasIndexedPropertiesInExternalArrayData()) {
+		length2 = size_t(matrix2Buff->GetIndexedPropertiesExternalArrayDataLength());
+		data2 = static_cast<float*>(matrix2Buff->GetIndexedPropertiesExternalArrayData());
+	}
+
+	CMf second(data2, rows2, cols2);
+	
+	Local<Object> matrixResBuff = args[6].As<Object>();
+	float *resRawData = nullptr;
+	size_t lengthRes = 0;
+
+	if (matrixResBuff->HasIndexedPropertiesInExternalArrayData()) {
+		lengthRes = size_t(matrixResBuff->GetIndexedPropertiesExternalArrayDataLength());
+		resRawData = static_cast<float*>(matrixResBuff->GetIndexedPropertiesExternalArrayData());
+	}
+	
+	Mf res(resRawData, rows1, cols2);
+	res = second * first;
+	Local<Boolean> b = NanNew(true);
+	NanReturnValue(b);
+}
+
 void Init(Handle<Object> exports) {
 	exports->Set(NanNew("MatMul"), NanNew<FunctionTemplate>(MatMul)->GetFunction());
+	exports->Set(NanNew("MatMul2"), NanNew<FunctionTemplate>(MatMul2)->GetFunction());
 }
 
 NODE_MODULE(addon, Init)
--- addon.js
+++ addon.js
@@ -8,12 +8,49 @@ console.log('Before ', mat1, mat2, res)
 console.log('Res:', addon.MatMul(2,2,mat1,2,2,mat2, res))
 console.log('After', mat1, mat2, res)
 
-
-/*var smalloc = require('smalloc');
+console.log('---------------------Before smalloc--------------------------')
+var smalloc = require('smalloc');
 var data1 = smalloc.alloc(4, smalloc.Types.Float);
 var data2 = smalloc.alloc(4, smalloc.Types.Float);
 var data3 = smalloc.alloc(4, smalloc.Types.Float);
 
-console.log('Before ', mat1, mat2, res)
-console.log('Res:', addon.MatMul(2,2,mat1,2,2,mat2, res))
-console.log('After', mat1, mat2, res) */
+data1[0] = 1.0;
+data1[1] = 1.0;
+data1[2] = 1.0;
+data1[3] = 1.0;
+
+console.log("-------------data1--------------------")
+console.log(data1[0], " ",data1[1]);
+console.log(data1[2], " ",data1[3]);
+
+data2[0] = 1.0;
+data2[1] = 1.0;
+data2[2] = 1.0;
+data2[3] = 1.0;
+
+console.log("-------------data2--------------------")
+console.log(data2[0], " ",data2[1]);
+console.log(data2[2], " ",data2[3]);
+
+data3[0] = 0.0;
+data3[1] = 0.0;
+data3[2] = 0.0;
+data3[3] = 0.0;
+
+console.log("-------------data3--------------------")
+console.log(data3[0], " ",data3[1]);
+console.log(data3[2], " ",data3[3]);
+
+//console.log('Before ', mat1, mat2, res);
+console.log('Res:', addon.MatMul2(2,2,data1,2,2,data2, data3));
+console.log('After');
+
+console.log("-------------data1--------------------")
+console.log(data1[0], " ",data1[1]);
+console.log(data1[2], " ",data1[3]);
+console.log("-------------data2--------------------")
+console.log(data2[0], " ",data2[1]);
+console.log(data2[2], " ",data2[3]);
+console.log("-------------data3--------------------")
+console.log(data3[0], " ",data3[1]);
+console.log(data3[2], " ",data3[3]);
